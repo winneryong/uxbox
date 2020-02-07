4d38f1b14d8e73b84e529b84d714f60deaec2bfa
Andrey Antukh <niwi@niwi.nz>
2020-02-07 18:43:27 +0100

    WIP 1

--- backend/resources/migrations/0001.main.sql
+++ backend/resources/migrations/0001.main.sql
@@ -1,8 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
--- Modified At
-
 CREATE FUNCTION update_modified_at()
   RETURNS TRIGGER AS $updt$
   BEGIN
